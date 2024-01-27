363b893ad137624e3bb7419b814d6b9471f1ef54
AmjadSajjad <amjadsajjadstk@gmail.com>
2024-01-27 09:55:27 +0600

    AddModal and NoTaskFound Components is Added but still there is no interactivity is settled.

---
+++ src/Components/Task/NoTaskFound.jsx
@@ -0,0 +1,11 @@
+
+const NoTaskFound = () => {
+    return (
+        <div>
+            <p className="text-3xl font-medium">No Task Found, Please Add Some Task What You Desired!</p>
+            
+        </div>
+    );
+};
+
+export default NoTaskFound;
